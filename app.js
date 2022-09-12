e045dc73b7678cfcad66ee9bbf78b435ceb95d1b
Kaushik Biswas <kbutsho@gmail.com>
2022-09-12 20:18:34 +0600

    update

--- app.js
+++ app.js
@@ -2,9 +2,26 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 
+//middleware
 app.use(cors());
 app.use(express.json());
-app.use(express.static("public"));
-app.use(express.urlencoded({ extended: false }));
+// app.use(express.static("public"));
+// app.use(express.urlencoded({ extended: false }));
+
+
+//schema design
+
+app.get('/', (req, res) => {
+    res.status(200).json({
+        'status': true,
+        'message': 'Index api'
+    });
+});
+app.all('*', (req, res) => {
+    res.status(404).json({
+        'status': false,
+        'message': 'page not found',
+    });
+});
 
 module.exports = app;
--- utils/dbConnection.js
+++ utils/dbConnection.js
@@ -1,6 +1,17 @@
 const { magenta, bold } = require('colorette');
+const mongoose = require('mongoose');
+require('dotenv').config(); 
 
-const dbConnection = ()=>{
-    console.log(magenta(bold("Database connected!")));
+const dbConnection = () => {
+    mongoose.connect(process.env.DATABASE_LOCAL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    }, (err) => {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log(magenta(bold("Database connected successfully!")));
+        }
+    }); 
 }
 module.exports = dbConnection;      
